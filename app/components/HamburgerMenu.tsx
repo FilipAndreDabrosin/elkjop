a82309b461457d9c6a33a5cb1dfd43c11a538c19
Filip Andre Dabrosinn <filipdabrosin@gmail.com>
2025-09-01 15:24:07 +0200

    Navbar progress, needs an update to the spacing

--- app/components/HamburgerMenu.tsx
+++ app/components/HamburgerMenu.tsx
@@ -1,23 +1,53 @@
-import { slide as Menu } from 'react-burger-menu'
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
-class Example extends React.Component {
-  showSettings (event) {
-    event.preventDefault();
-    .
-    .
-    .
-  }
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  render () {
-    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
-    return (
-      <Menu right>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      </Menu>
-    );
-  }
-}
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative">
+      <button
+        onClick={handleClick}
+        className="flex flex-col justify-center items-center"
+      >
+        <span
+          className={`bg-gray-800 block transition-all duration-300 ease-out 
+                      h-0.5 w-6 rounded-sm ${
+                        isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+                      }`}
+        ></span>
+        <span
+          className={`bg-gray-800 block transition-all duration-300 ease-out 
+                      h-0.5 w-6 rounded-sm my-0.5 ${
+                        isOpen ? "opacity-0" : "opacity-100"
+                      }`}
+        ></span>
+        <span
+          className={`bg-gray-800 block transition-all duration-300 ease-out 
+                      h-0.5 w-6 rounded-sm ${
+                        isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+                      }`}
+        ></span>
+      </button>
+      {isOpen && (
+        <div className="absolute left-0 mt-2 w-40 bg-white shadow-lg rounded-lg py-2 z-50">
+          <a href="/" className="block px-4 py-2 hover:bg-gray-100">
+            Home
+          </a>
+          <a href="/about" className="block px-4 py-2 hover:bg-gray-100">
+            About
+          </a>
+          <a href="/contact" className="block px-4 py-2 hover:bg-gray-100">
+            Contact
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,23 +1,43 @@
 import Image from "next/image";
 import SearchBar from "./SearchBar";
+import HamburgerMenu from "./HamburgerMenu";
+
 
 const NavBar = () => {
   return (
-    <nav>
-      <div className="drop-shadow-md justify-between flex px-4 align-center">
-        <div>
+    <nav className="">
+      <div className="shadow-xs justify-between flex py-2 px-4 align-center">
+        <a href="/">
           <Image
             src="/Elkjop_logo_blue.png"
             alt="Elkjøp Logo"
             width={150}
             height={50}
           />
-        </div>
+        </a>
         <div className="flex items-center justify-items-start">
           <SearchBar />
         </div>
-        <div>
-
+        <div className="align-center flex items-center">
+        <HamburgerMenu />
+        </div>
+        <div className="flex flex-col items-center justify-end">
+            <Image 
+            src="/person-outline.svg"
+            alt="Bruker ikon"
+            width={30}
+            height={30}
+            />
+            <p className="pl-2 pr-4 text-sm text-[#041753]">Logg inn</p>
+        </div>
+        <div className="flex flex-col items-center justify-end">
+            <Image 
+            src="/cart-outline.svg"
+            alt="Handlekurv ikon"
+            width={30}
+            height={30}
+            />
+            <p className="pl-2 pr-4 text-sm text-[#041753]">Handlekurv</p>
         </div>
       </div>
     </nav>
